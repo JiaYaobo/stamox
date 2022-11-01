ee947c31b004623bd61ffd560da82d94ae23ac17
jiayaobo <januzaj2011@163.com>
2022-11-01 13:03:12 +0800

    update

--- sax/base/model.py
+++ sax/base/model.py
@@ -6,9 +6,33 @@ class Model(eqx.Module):
 
     def __init__(self) -> None:
         super().__init__()
+        
+    def __call__(cls, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
 
-    def set_mode(self):
-        pass
+class ParamModel(eqx.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
 
     def __call__(cls, *args, **kwargs):
         return super().__call__(*args, **kwargs)
+
+
+
+class NonParamModel(eqx.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def __call__(cls, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
+
+
+class SemiParamModel(ParamModel, NonParamModel):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __call__(cls, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
+    
