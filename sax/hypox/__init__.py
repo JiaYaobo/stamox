451e4518f979c9572730a66f230a11ced3de9832
jiayaobo <januzaj2011@163.com>
2022-11-03 20:38:32 +0800

    update

--- sax/hypox/__init__.py
+++ sax/hypox/__init__.py
@@ -1,3 +1,3 @@
-from sax.hypox._t_test import t_test
-from sax.hypox._p_test import p_test
-from sax.hypox._durbin_waston_test import durbin_waston
+from sax.hypox._t import t_test
+from sax.hypox._p import p_test
+from sax.hypox._durbin_waston import durbin_waston
