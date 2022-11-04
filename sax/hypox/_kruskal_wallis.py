7953945c3da0873d82b290e74ea7db5995c77e95
jiayaobo <januzaj2011@163.com>
2022-11-04 22:27:33 +0800

    update

---
+++ sax/hypox/_kruskal_wallis.py
@@ -0,0 +1,12 @@
+"""
+The Kruskal–Wallis test by ranks, Kruskal–Wallis H test (named after William Kruskal and W. Allen Wallis), 
+or one-way ANOVA on ranks[1] is a non-parametric method for testing whether samples originate from the same distribution. 
+It is used for comparing two or more independent samples of equal or different sample sizes. 
+It extends the Mann–Whitney U test, which is used for comparing only two groups. 
+The parametric equivalent of the Kruskal–Wallis test is the one-way analysis of variance (ANOVA).
+"""
+
+
+
+def _kruskal_wallis():
+    pass
---
+++ sax/math/rank.py
@@ -0,0 +1,11 @@
+import jax.numpy as jnp
+
+
+def rank(x):
+    pass
+
+def dense_rank(x):
+    pass
+
+def tiecorrect(rankvals):
+    pass
