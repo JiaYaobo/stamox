b6b91b4410c220522eeb2f63921e2c90e5b5f504
jiayaobo <januzaj2011@163.com>
2022-11-05 18:59:42 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = pathlib.Path(__file__).parent
 
 VERSION = '0.0.1'
 PACKAGE_NAME = 'stamox'
-AUTHOR = 'The stamox Authors'
+AUTHOR = 'The Stamox Authors'
 URL = 'https://github.com/jiayaobo/sax'
 
 LICENSE = 'MIT'
@@ -13,7 +13,7 @@ DESCRIPTION = 'High Performance Statistics Library'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
-INSTALL_REQUIRES = ['numpy', 'jax>=0.3.23', 'jaxlib>=0.3.15', 'equinox>=0.8.0', 'chex>=0.1.5', 'pandas>=1.5.0', 'tensorflow-probability>=0.18.0']
+INSTALL_REQUIRES = ['numpy>=1.23', 'pandas>=1.5.0', 'jax>=0.3.23', 'jaxlib>=0.3.15', 'equinox>=0.8.0', 'chex>=0.1.5', 'pandas>=1.5.0', 'tensorflow-probability>=0.18.0', 'numpyro>=0.10.1']
 TESTS_REQUIRES = ['pytest']
 
 setup(
