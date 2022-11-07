1864bc6dde4edcbc3797ee95280c97d8e4e0155d
jiayaobo <januzaj2011@163.com>
2022-11-07 21:44:02 +0800

    update

---
+++ stamox/corrx/_pearsonr.py
@@ -0,0 +1,37 @@
+import functools as ft
+
+import jax.numpy as jnp
+from jax import jit, vmap, scipy
+from stamox.distrix import pbeta
+
+
+def pearsonr(x, y, alternative='two-sided'):
+    x = jnp.asarray(x)
+    y = jnp.asarray(y)
+
+    return _pearsonr(x, y, alternative)
+
+
+@ft.partial(jit, static_argnames=('alternative', ))
+def _pearsonr(x, y, alternative='two-sided'):
+    n = x.size
+
+    xm = x - x.mean()
+    ym = y - y.mean()
+
+    normxm = jnp.linalg.norm(xm)
+    normym = jnp.linalg.norm(ym)
+
+    r = jnp.dot(xm/normxm, ym/normym)
+
+    r = jnp.maximum(jnp.minimum(r, 1.0), -1.0)
+    ab = n / 2 - 1
+
+    if alternative == 'two-sided':
+        prob = 2*pbeta(0.5*(1 - jnp.abs(r)), ab, ab)
+    elif alternative == 'less':
+        prob = 1 - pbeta(0.5*(1 - jnp.abs(r)), ab, ab)
+    else:
+        prob = pbeta(0.5*(1 - jnp.abs(r)), ab, ab)
+
+    return r, prob
