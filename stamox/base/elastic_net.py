19d3fadd2abe912e7e82fbc7e77144fb2d4ac648
jiayaobo <januzaj2011@163.com>
2023-03-16 16:59:08 +0800

    wip

--- stamox/base/elastic_net.py
+++
@@ -1,2 +0,0 @@
-def _fit_elastic_net():
-    pass
--- stamox/core/datax.py
+++ stamox/core/datax.py
@@ -9,11 +9,6 @@ from pandas import DataFrame
 
 class DataX(DataFrame):
 
-    # def __init__(self, *args, **kwargs) -> None:
-    #     super(DataX, self).__init__(*args, **kwargs)    
-    #     self._fun_tree = []
-    # def __rshift__(self, func):
-    #     self._fun_tree.append(func)
     def __init__(self) -> None:
         super().__init__()
     
