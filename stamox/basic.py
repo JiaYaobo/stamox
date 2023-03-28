e9c2ccfcd4e6d00d22522f59d5d996523269ceec
jiayaobo <januzaj2011@163.com>
2023-03-28 17:01:17 +0800

    update scale function

--- stamox/basic.py
+++ stamox/basic.py
@@ -1,16 +1,21 @@
 import jax.numpy as jnp
+from jax import vmap
 
 from .core import make_partial_pipe
 
 
-mean = make_partial_pipe(jnp.mean, name='mean')
-std = make_partial_pipe(jnp.std, name='std')
-var = make_partial_pipe(jnp.var, name='var')
-median = make_partial_pipe(jnp.median, name='median')
+mean = make_partial_pipe(jnp.mean, name="mean")
+std = make_partial_pipe(jnp.std, name="std")
+var = make_partial_pipe(jnp.var, name="var")
+median = make_partial_pipe(jnp.median, name="median")
 
-@make_partial_pipe(name='scale')
+
+@make_partial_pipe(name="scale")
 def scale(x, axis=0):
     # calculate standardized x along axis
-    return (x - mean(x, axis=axis)) / std(x, axis=axis)
-
-
+    _mean = mean(x, axis=axis)
+    _std = std(x, axis=axis, ddof=1)
+    _scaled = vmap(lambda a, b, c: (a - b) / c, in_axes=(axis, None, None))(
+        x, _mean, _std
+    )
+    return _scaled
