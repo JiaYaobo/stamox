283253a3a3f1589ae7a402e7ecebc5ee34e9b9a2
jiayaobo <januzaj2011@163.com>
2023-04-04 22:34:32 +0800

    add bootstrap and jackknife

--- tests/sample/test_bootstrap.py
+++ tests/sample/test_bootstrap.py
@@ -3,8 +3,9 @@ import jax.random as jrandom
 from absl.testing import absltest
 from jax._src import test_util as jtest
 
+from stamox.basic import mean
 from stamox.core import Pipeable
-from stamox.sample import bootstrap_sample, jackknife_sample
+from stamox.sample import bootstrap_sample, bootstrap
 
 
 class BootstrapSamplerTest(jtest.JaxTestCase):
@@ -15,18 +16,28 @@ class BootstrapSamplerTest(jtest.JaxTestCase):
         S = bootstrap_sample(X, 5, key=key)
         self.assertEqual(S.shape, (5, 1000, 3))
     
-    def test_partial_boostrap_sampler(self):
+    def test_partial_pipe_boostrap_sampler(self):
         key = jrandom.PRNGKey(20010813)
         X = jrandom.normal(key=key, shape=(1000, 3))
         h = Pipeable(X) >> bootstrap_sample(num_samples=5, key=key)
         S = h()
         self.assertEqual(S.shape, (5, 1000, 3))
     
-    def test_jackknife_sampler(self):
+    def test_bootstrap(self):
         key = jrandom.PRNGKey(20010813)
         X = jrandom.normal(key=key, shape=(1000, 3))
-        S = jackknife_sample(X, axis=0)
-        self.assertEqual(S.shape, (1000, 999, 3))
+        S = bootstrap(X, mean, 100, key=key)
+        self.assertAllClose(mean(S), mean(X), atol=1e-2)
+    
+
+    def test_partial_pipe_bootstrap(self):
+        key = jrandom.PRNGKey(20010813)
+        X = jrandom.normal(key=key, shape=(1000, 3))
+        h = Pipeable(X) >> bootstrap(call=mean, num_samples=100, key=key)
+        S = h()
+        self.assertAllClose(mean(S), mean(X), atol=1e-2)
+    
+    
 
 
 
