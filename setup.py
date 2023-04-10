58a657e596cd94597ad05daa58e119659de1da77
jiayaobo <januzaj2011@163.com>
2023-04-10 17:43:09 +0800

    update for v0.1.0a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = "0.0.2"
+VERSION = "0.1.0"
 PACKAGE_NAME = "stamox"
 AUTHOR = "Jia Yaobo"
 URL = "https://github.com/jiayaobo/stamox"
