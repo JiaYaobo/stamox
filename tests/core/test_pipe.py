53b4253a760cdde40a8ad02d42093355346835d2
jiayaobo <januzaj2011@163.com>
2023-04-17 18:10:59 +0800

    modify pipe test

--- tests/core/test_pipe.py
+++ tests/core/test_pipe.py
@@ -2,7 +2,7 @@
 from absl.testing import absltest
 from jax._src import test_util as jtest
 
-from stamox.core import make_pipe, Pipe
+from stamox.core import make_partial_pipe, make_pipe, Pipe
 
 
 class PipeTest(jtest.JaxTestCase):
@@ -51,6 +51,28 @@ class PipeTest(jtest.JaxTestCase):
         pipe = f >> g >> f >> g >> f >> g
         self.assertEqual(len(pipe[0:1]), 1)
         self.assertEqual(pipe[0:1][0].func, f.func)
+
+    def test_create_partial_pipe_class(self):
+        @make_partial_pipe
+        def f(x, y):
+            return x + y
+
+        def g(x):
+            return x * 2
+
+        pipe = f(y=2) >> g
+        self.assertIsInstance(pipe, Pipe)
+
+    def test_evaluate_partial_pipe_class(self):
+        @make_partial_pipe
+        def f(x, y):
+            return x + y
+
+        def g(x):
+            return x * 2
+
+        pipe1 = f(y=2) >> g
+        self.assertEqual(pipe1(1), 6)
 
 
 if __name__ == "__main__":
