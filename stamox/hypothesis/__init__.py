e60c603fbb32695056dafdc82294787a4e7e029a
jiayaobo <januzaj2011@163.com>
2023-04-21 18:12:28 +0800

    remove shapiro test temporarily for lax compatible

--- stamox/hypothesis/__init__.py
+++ stamox/hypothesis/__init__.py
@@ -2,7 +2,6 @@ from stamox.hypothesis._bartlett import bartlett_test, BartlettTest
 from stamox.hypothesis._base import HypoTest
 from stamox.hypothesis._durbin_watson import durbin_watson_test, DurbinWatsonTest
 from stamox.hypothesis._friedman import friedman_test, FriedmanTest
-from stamox.hypothesis._shapiro_wilk import shapiro_wilk_test, ShapiroWilkTest
 
 
 __all__ = [
@@ -13,6 +12,4 @@ __all__ = [
     "DurbinWatsonTest",
     "friedman_test",
     "FriedmanTest",
-    "shapiro_wilk_test",
-    "ShapiroWilkTest",
 ]
